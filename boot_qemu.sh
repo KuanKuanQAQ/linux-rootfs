791f0f10518bfab8f5f68a1f78d7fdbfdf2b840b
KuanKuanQAQ <leaker233@icloud.com>
2023-07-06 12:35:25 +0000

    Keep empty dir.

---
+++ boot_qemu.sh
@@ -0,0 +1,7 @@
+sh mkrootfs.sh
+qemu-system-aarch64 -s -S\
+    -machine virt -cpu cortex-a57 \
+    -kernel ~/kernel-openEuler-22.03-LTS/arch/arm64/boot/Image \
+    -initrd ~/linux-rootfs/initramfs.cpio.gz \
+    -nographic \
+    -append "nokaslr console=ttyAMA0"
